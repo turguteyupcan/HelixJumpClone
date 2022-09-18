db2b059514d091dece21f401a9660b235fa7ca4a
Eyüp Can Turgut <48491124+turguteyupcan@users.noreply.github.com>
2022-09-18 22:32:27 +0300

    Gorsel iyilestirme
    Topun sektigi yere splash efekti eklendi ve oynanis biraz daha iyi hale geldi.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,6 +6,7 @@ public class Ball : MonoBehaviour
 {
     private Rigidbody rb;
     public float jumpForce;
+    public GameObject splashPrefab;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,8 +20,12 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        //.AddForce(Vector3.up * jumpForce);
+        //AddForce(Vector3.up * jumpForce);
         rb.velocity = Vector3.up * jumpForce;
+
+        GameObject splash = Instantiate(splashPrefab, transform.position + new Vector3(0, -0.2f, 0), Quaternion.Euler(90, 0, 0));
+        splash.transform.SetParent(collision.gameObject.transform);
+
         string materialName = collision.gameObject.GetComponent<MeshRenderer>().material.name;
         Debug.Log(materialName);
 
